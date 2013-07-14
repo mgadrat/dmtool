95c59ab9a616c06e0ca9422dbe6417a25c8a6281
Matthieu Gadrat <mgadrat@gmail.com>
2013-07-14 18:00:36 -0400

    Adding creatures and characters table and detect schema version

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -5,20 +5,20 @@
  */
 class Install extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/install
-	 */
-	public function index()
-	{
-		$this->load->model('DbScheme');
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *      http://example.com/index.php/install
+     */
+    public function index()
+    {
+        $this->load->model('DbScheme');
 
-		$data = array();
-		$data['message'] = "Application Installed";
-		$data['message_type'] = "success";
-		$this->load->view('home', $data);
-	}
+        $data = array();
+        $data['message'] = "Application Installed";
+        $data['message_type'] = "success";
+        $this->load->view('home', $data);
+    }
 
 }
--- application/models/DbScheme.php
+++ application/models/DbScheme.php
@@ -1,5 +1,9 @@
 <?php
 
+define('SCHEME_STATUS_ABSENT', 0);
+define('SCHEME_STATUS_OK', 1);
+define('SCHEME_STATUS_OUT_OF_DATE', 2);
+
 class DbScheme extends CI_Model {
 
     var $version = '0.0.0.0';
@@ -14,20 +18,40 @@ class DbScheme extends CI_Model {
         // Call the Model constructor
         parent::__construct();
 
-        if(!$this->is_scheme_installed()){
-            $this->create_scheme();
-        }
+        // Load dbforge && database
+        $this->load->dbforge();
+        $this->load->database();
+
+
+        var_dump( $this->get_scheme_status() );
+        // Create scheme
+        $this->create_scheme();
         
     }
     
     /**
      * Verify if the db scheme is already installed
      * 
-     * @return bool
+     * @return int the status of the database scheme
      */
-    private function is_scheme_installed()
+    private function get_scheme_status()
     {
-        return FALSE;
+        // Check if schema installed
+        $tables = $this->db->query('SHOW TABLES')->result();
+        if(empty($tables)){
+            return SCHEME_STATUS_ABSENT;
+        }
+
+        // Query schema version
+        $scheme_version = $this->db->get_where('system', array('variable' => 'scheme_version'))->result();
+
+        if(empty($scheme_version[0]->value)){
+            return SCHEME_STATUS_ABSENT;
+        }else if($scheme_version[0]->value != $this->version){
+            return SCHEME_STATUS_OUT_OF_DATE;
+        }
+
+        return SCHEME_STATUS_OK;
     }
 
     /**
@@ -37,13 +61,11 @@ class DbScheme extends CI_Model {
      */
     private function delete_current_scheme()
     {
-        // Load dbforge
-        $this->load->dbforge();
 
         // Load application scheme
         $tables = $this->get_scheme();
 
-        foreach($tables as $key => $table){
+        foreach($tables as $key => $table) {
             $this->dbforge->drop_table($key);
         }
 
@@ -55,27 +77,24 @@ class DbScheme extends CI_Model {
      * 
      * @return bool TRUE on success FALSE on failure
      */
-    private function create_scheme(){
+    private function create_scheme() {
 
         // (temp)
         // delete scheme
         $this->delete_current_scheme();
 
-        // Load dbforge
-        $this->load->dbforge();
-
         // Load application scheme
         $tables = $this->get_scheme();
 
         // Parse scheme and create each tables
-        foreach($tables as $key => $table){
+        foreach($tables as $key => $table) {
 
             $this->dbforge->add_field($table['scheme']);
 
             // if a primary is defined use it, if not use default CI implementation
-            if(isset($table['primary'])){
+            if(isset($table['primary'])) {
                 $this->dbforge->add_key($table['primary'], TRUE);
-            }else{
+            } else {
                 $this->dbforge->add_field('id');
             }
 
@@ -83,6 +102,13 @@ class DbScheme extends CI_Model {
             $this->dbforge->create_table($key);
 
         }
+
+        $data = array(
+           'variable' => 'scheme_version' ,
+           'value'    => $this->version,
+        );
+
+        $this->db->insert('system', $data); 
 
         return TRUE;
     }
@@ -93,9 +119,23 @@ class DbScheme extends CI_Model {
      * 
      * @return Array Full db scheme of the application
      */
-    private function get_scheme(){
+    private function get_scheme() {
 
         return array(
+            // System information and configuration
+            'system' => array(
+                'scheme' => array(
+                    'variable' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                    ),
+                    'value' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                    ),
+                ),
+                'primary' => 'variable',
+            ),
             'node' => array(
                 'scheme' => array(
                     'nid' => array(
@@ -128,7 +168,131 @@ class DbScheme extends CI_Model {
                     ),
                 ),
                 'primary' => 'nid',
-            ),// end of node table
+            ), // end of node table
+            'creatures' => array(
+                'scheme' => array(
+                    // Node id
+                    'nid' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                         'unsigned'   => TRUE,
+                    ),
+                    // level (characters and consters)
+                    'level' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                         'unsigned'   => TRUE,
+                    ),
+                    // maximum health point
+                    'max_hp' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                         'unsigned'   => TRUE,
+                    ),
+                    // health point
+                    'hp' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                    ),
+                    // maximum action point
+                    'max_ap' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                         'unsigned'   => TRUE,
+                    ),
+                    // action point
+                    'ap' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                         'unsigned'   => TRUE,
+                    ),
+                ),
+            ), // end of creatures table
+            'characters' => array(
+                'scheme' => array(
+                    // Node Id
+                    'nid' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                         'unsigned'   => TRUE,
+                    ),
+                    // Experience accumulated
+                    'xp' => array(
+                         'type'       => 'INT',
+                         'constraint' => 11, 
+                         'unsigned'   => TRUE,
+                    ),
+                    // Race
+                    'race' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                    ),
+                    // Size (S, M, L)
+                    'size' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                    ),
+                    // Character class
+                    'class' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                    ),
+                    // Paragon Path
+                    'paragon' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // Epic Destiny
+                    'epic' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // Gods
+                    'deity' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // How old
+                    'age' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // Male / Female / Other...
+                    'gender' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // Vertically
+                    'height' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // Horizontally
+                    'weight' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // Good / evil
+                    'alignment' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                    // Company or affiliation in the game
+                    'affiliation' => array(
+                         'type'       => 'VARCHAR',
+                         'constraint' => '255',
+                         'null'       => TRUE,
+                    ),
+                ),
+            ), // end of characters table
         );
 
     }
